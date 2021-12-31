6565015db16228b0c0d24499bb35620137a8e2d1
Frankenstein <frankenstein@pop-os.localdomain>
2021-12-31 08:05:50 +0530

    backeend first commit

---
+++ schemas/comment.js
@@ -0,0 +1,17 @@
+export default{
+    name:'comment',
+    title:'comment',
+    type:'document',
+    field:[
+        {
+            name:'postedBy',
+            title:'PostedBy',
+            type:'postedBy'
+        },
+        {
+            name:'comment',
+            title:'Comment',
+            type:'string'
+        },
+    ]
+}
---
+++ schemas/save.js
@@ -0,0 +1,17 @@
+export default{
+    name:'save',
+    title:'Save',
+    type:'document',
+    field:[
+        {
+            name:'postedBy',
+            title:'PostedBy',
+            type:'postedBy'
+        },
+        {
+            name:'userId',
+            title:'UserId',
+            type:'string'
+        },
+    ]
+}
